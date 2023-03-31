2e3e987e391d68abf491ce2a3c444791ab140887
otenav <ola.toft-eriksen@nav.no>
2023-03-31 10:07:25 +0200

    Bruker mb-3 i stedet for section

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -17,57 +17,50 @@ export default function kontaktOss() {
                         Kontakt oss
                     </Heading>
 
-                    <section className="arb-article-section">
-                        <Heading size="medium" level="2" spacing>
-                            Jobbsøker
-                        </Heading>
-                        <ul>
-                            <li>
-                                <BodyLong>
-                                    Se vår{" "}
-                                    <Link href="https://www.nav.no/teknisk">
-                                        teknisk brukerstøtteside for privatpersoner
-                                    </Link>
-                                </BodyLong>
-                            </li>
-                            <li>
-                                <BodyLong>
-                                    Får du oppfølging fra NAV? Du kan{" "}
-                                    <Link href="https://www.nav.no/person/kontakt-oss/">kontakte NAV på telefon.</Link>
-                                </BodyLong>
-                            </li>
-                        </ul>
-                    </section>
+                    <Heading size="medium" level="2" spacing>
+                        Jobbsøker
+                    </Heading>
+                    <ul className="mb-1">
+                        <li>
+                            <BodyLong>
+                                Se vår{" "}
+                                <Link href="https://www.nav.no/teknisk">
+                                    teknisk brukerstøtteside for privatpersoner
+                                </Link>
+                            </BodyLong>
+                        </li>
+                        <li>
+                            <BodyLong>
+                                Får du oppfølging fra NAV? Du kan{" "}
+                                <Link href="https://www.nav.no/person/kontakt-oss/">kontakte NAV på telefon.</Link>
+                            </BodyLong>
+                        </li>
+                    </ul>
 
-                    <section className="arb-article-section">
-                        <Heading size="medium" level="2" spacing>
-                            Arbeidsgiver
-                        </Heading>
-                        <ul>
-                            <li>
-                                <BodyLong>
-                                    Se vår{" "}
-                                    <Link href="https://www.nav.no/teknisk-bedrift">
-                                        teknisk brukerstøtteside for bedrifter
-                                    </Link>
-                                </BodyLong>
-                            </li>
-                            <li>
-                                <BodyLong>
-                                    Arbeidsgivertelefonen i NAV tlf. 55 55 33 36. Hverdager 0900–1500. Svarer på
-                                    spørsmål om rekruttering, sykefraværsoppfølging, refusjoner, permittering og
-                                    omstilling m.m.
-                                </BodyLong>
-                            </li>
-                            <li>
-                                <BodyLong>
-                                    <Link href="https://arbeidsgiver.nav.no/kontakt-oss/">
-                                        Kontakt NAV om rekruttering
-                                    </Link>
-                                </BodyLong>
-                            </li>
-                        </ul>
-                    </section>
+                    <Heading size="medium" level="2" spacing>
+                        Arbeidsgiver
+                    </Heading>
+                    <ul>
+                        <li>
+                            <BodyLong>
+                                Se vår{" "}
+                                <Link href="https://www.nav.no/teknisk-bedrift">
+                                    teknisk brukerstøtteside for bedrifter
+                                </Link>
+                            </BodyLong>
+                        </li>
+                        <li>
+                            <BodyLong>
+                                Arbeidsgivertelefonen i NAV tlf. 55 55 33 36. Hverdager 0900–1500. Svarer på spørsmål om
+                                rekruttering, sykefraværsoppfølging, refusjoner, permittering og omstilling m.m.
+                            </BodyLong>
+                        </li>
+                        <li>
+                            <BodyLong>
+                                <Link href="https://arbeidsgiver.nav.no/kontakt-oss/">Kontakt NAV om rekruttering</Link>
+                            </BodyLong>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </LayoutArticles>
