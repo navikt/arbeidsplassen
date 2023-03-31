8d4a7f16187ae55f602cf77453c08a9afa38b05e
otenav <ola.toft-eriksen@nav.no>
2023-03-31 10:33:35 +0200

    Bruker mb-3 i stedet for section

--- src/pages/jobbe-i-utlandet.js
+++ src/pages/jobbe-i-utlandet.js
@@ -8,7 +8,7 @@ export default function jobbeiUtlandet() {
         <LayoutArticles>
             <div className="page-margin-top-and-bottom">
                 <div className="arb-article">
-                    <div className="mb-24">
+                    <div className="mb-2">
                         <Link href="/">
                             <ChevronLeftIcon aria-hidden="true" />
                             Til forsiden
@@ -28,30 +28,26 @@ export default function jobbeiUtlandet() {
                         <IllustrationArticle className="arb-article-figure-pull-outside" />
                     </div>
 
-                    <section className="arb-article-section">
-                        <Heading size="large" level="2" spacing>
-                            Hva kan jeg gjøre på EURES-portalen?
-                        </Heading>
-                        <BodyLong spacing>
-                            På portalen kan du søke etter ledige stillinger. Du kan også gjøre CV-en din tilgjengelig
-                            for arbeidsgivere som søker nye medarbeidere.
-                        </BodyLong>
-                        <BodyLong>
-                            Dersom du er statsborger i et EU/EØS-land eller Sveits kan du eksportere CV-en du har på
-                            arbeidsplassen.no til EURES-portalen.
-                        </BodyLong>
-                    </section>
+                    <Heading size="large" level="2" spacing>
+                        Hva kan jeg gjøre på EURES-portalen?
+                    </Heading>
+                    <BodyLong spacing>
+                        På portalen kan du søke etter ledige stillinger. Du kan også gjøre CV-en din tilgjengelig for
+                        arbeidsgivere som søker nye medarbeidere.
+                    </BodyLong>
+                    <BodyLong className="mb-3">
+                        Dersom du er statsborger i et EU/EØS-land eller Sveits kan du eksportere CV-en du har på
+                        arbeidsplassen.no til EURES-portalen.
+                    </BodyLong>
 
-                    <section>
-                        <LinkPanel
-                            className="arb-secondary-bg-text arb-link-panel"
-                            href="https://ec.europa.eu/eures/public/index_no"
-                        >
-                            <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
-                                Gå til EURES-portalen
-                            </LinkPanel.Title>
-                        </LinkPanel>
-                    </section>
+                    <LinkPanel
+                        className="arb-secondary-bg-text arb-link-panel"
+                        href="https://ec.europa.eu/eures/public/index_no"
+                    >
+                        <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
+                            Gå til EURES-portalen
+                        </LinkPanel.Title>
+                    </LinkPanel>
                 </div>
             </div>
         </LayoutArticles>
