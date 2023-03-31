baa8849de4b7224275aa5cc7ab30ce88d937672d
otenav <ola.toft-eriksen@nav.no>
2023-03-31 10:32:44 +0200

    Bruker mb-3 i stedet for section

--- src/pages/articles/person/tips-til-jobbsoknaden.js
+++ src/pages/articles/person/tips-til-jobbsoknaden.js
@@ -8,7 +8,7 @@ export default function tipsTilJobbsoknaden() {
         <LayoutArticles>
             <div className="page-margin-top-and-bottom">
                 <div className="arb-article">
-                    <div className="mb-24">
+                    <div className="mb-2">
                         <Link href="/">
                             <ChevronLeftIcon aria-hidden="true" />
                             Til forsiden
@@ -29,7 +29,7 @@ export default function tipsTilJobbsoknaden() {
                     </div>
 
                     <section className="arb-article-subsection">
-                        <Heading size="medium" level="2">
+                        <Heading size="large" level="2" spacing>
                             Søknaden er et svar på annonsen
                         </Heading>
                         <BodyLong spacing>
@@ -41,7 +41,7 @@ export default function tipsTilJobbsoknaden() {
                     </section>
 
                     <section className="arb-article-subsection">
-                        <Heading size="small" level="3" spacing>
+                        <Heading size="medium" level="3" spacing>
                             Dette bør du ha med
                         </Heading>
                         <ul>
@@ -66,73 +66,63 @@ export default function tipsTilJobbsoknaden() {
                         </ul>
                     </section>
 
-                    <section className="arb-article-section">
-                        <Heading size="small" level="3" spacing>
-                            Flere tips
-                        </Heading>
-                        <ul>
-                            <li>
-                                <BodyLong>
-                                    Ta gjerne kontakt med arbeidsgiveren før du sender inn søknad. Det er lurt å
-                                    planlegge hva du skal spørre om før du tar kontakt.
-                                </BodyLong>
-                            </li>
-                            <li>
-                                <BodyLong>Søknaden skal være kort og målrettet.</BodyLong>
-                            </li>
-                            <li>
-                                <BodyLong>Søknaden må se ryddig ut og bør maksimalt være på en side.</BodyLong>
-                            </li>
-                            <li>
-                                <BodyLong>Fokuser på hva du kan.</BodyLong>
-                            </li>
-                            <li>
-                                <BodyLong>Unngå skrivefeil.</BodyLong>
-                            </li>
-                            <li>
-                                <BodyLong>
-                                    Når søknaden og CV-en er sendt må du være tilgjengelig på telefon og sjekke e-posten
-                                    din ofte.
-                                </BodyLong>
-                            </li>
-                            <li>
-                                <BodyLong>Skriv en ny søknad til hver jobb du søker.</BodyLong>
-                            </li>
-                            <li>
-                                <BodyLong>
-                                    Har du ikke hørt noe to-tre uker etter søknadsfristen, kan du kontakte
-                                    arbeidsgiveren og høre hvor langt de er kommet i prosessen.
-                                </BodyLong>
-                            </li>
-                        </ul>
-                        <BodyLong>
-                            Husk å sende CV sammen med jobbsøknaden. Du kan registrere CV kostnadsfritt på
-                            arbeidsplassen.no.
-                        </BodyLong>
-                    </section>
+                    <Heading size="medium" level="3" spacing>
+                        Flere tips
+                    </Heading>
+                    <ul>
+                        <li>
+                            <BodyLong>
+                                Ta gjerne kontakt med arbeidsgiveren før du sender inn søknad. Det er lurt å planlegge
+                                hva du skal spørre om før du tar kontakt.
+                            </BodyLong>
+                        </li>
+                        <li>
+                            <BodyLong>Søknaden skal være kort og målrettet.</BodyLong>
+                        </li>
+                        <li>
+                            <BodyLong>Søknaden må se ryddig ut og bør maksimalt være på en side.</BodyLong>
+                        </li>
+                        <li>
+                            <BodyLong>Fokuser på hva du kan.</BodyLong>
+                        </li>
+                        <li>
+                            <BodyLong>Unngå skrivefeil.</BodyLong>
+                        </li>
+                        <li>
+                            <BodyLong>
+                                Når søknaden og CV-en er sendt må du være tilgjengelig på telefon og sjekke e-posten din
+                                ofte.
+                            </BodyLong>
+                        </li>
+                        <li>
+                            <BodyLong>Skriv en ny søknad til hver jobb du søker.</BodyLong>
+                        </li>
+                        <li>
+                            <BodyLong>
+                                Har du ikke hørt noe to-tre uker etter søknadsfristen, kan du kontakte arbeidsgiveren og
+                                høre hvor langt de er kommet i prosessen.
+                            </BodyLong>
+                        </li>
+                    </ul>
+                    <BodyLong className="mb-3">
+                        Husk å sende CV sammen med jobbsøknaden. Du kan registrere CV kostnadsfritt på
+                        arbeidsplassen.no.
+                    </BodyLong>
 
-                    <section className="arb-article-section">
-                        <Heading size="medium" level="2" spacing>
-                            Åpen søknad
-                        </Heading>
-                        <BodyLong spacing>
-                            Er det steder du kunne tenke deg å jobbe? Du kan sende en jobbsøknad selv om arbeidsgiveren
-                            ikke har lyst ut noen ledige stilliger. Dette kalles en åpen søknad. En åpen søknad er lik
-                            en vanlig søknad, men vær tydelig på hvorfor du ønsker å jobbe akkurat der. Lykke til med
-                            jobbsøkingen.
-                        </BodyLong>
-                    </section>
+                    <Heading size="large" level="2" spacing>
+                        Åpen søknad
+                    </Heading>
+                    <BodyLong className="mb-3">
+                        Er det steder du kunne tenke deg å jobbe? Du kan sende en jobbsøknad selv om arbeidsgiveren ikke
+                        har lyst ut noen ledige stilliger. Dette kalles en åpen søknad. En åpen søknad er lik en vanlig
+                        søknad, men vær tydelig på hvorfor du ønsker å jobbe akkurat der. Lykke til med jobbsøkingen.
+                    </BodyLong>
 
-                    <section className="arb-article-section">
-                        <LinkPanel
-                            className="arb-primary-bg-text arb-link-panel"
-                            href="https://arbeidsplassen.nav.no/cv/"
-                        >
-                            <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
-                                Opprett en CV
-                            </LinkPanel.Title>
-                        </LinkPanel>
-                    </section>
+                    <LinkPanel className="arb-primary-bg-text arb-link-panel" href="https://arbeidsplassen.nav.no/cv/">
+                        <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
+                            Opprett en CV
+                        </LinkPanel.Title>
+                    </LinkPanel>
                 </div>
             </div>
         </LayoutArticles>
