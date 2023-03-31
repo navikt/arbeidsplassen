4b5a437f0a9e6002271e1822289fdfc89ddcbba6
otenav <ola.toft-eriksen@nav.no>
2023-03-31 10:23:11 +0200

    Bruker mb-3 i stedet for section

--- src/pages/articles/person/jobbtreff-person.js
+++ src/pages/articles/person/jobbtreff-person.js
@@ -8,7 +8,7 @@ export default function jobbtreffPerson() {
         <LayoutArticles>
             <div className="page-margin-top-and-bottom">
                 <div className="arb-article">
-                    <div className="mb-24">
+                    <div className="mb-2">
                         <Link href="/">
                             <ChevronLeftIcon aria-hidden="true" />
                             Til forsiden
@@ -28,44 +28,36 @@ export default function jobbtreffPerson() {
                         <IllustrationArticle className="arb-article-figure-pull-outside" />
                     </div>
 
-                    <section className="arb-article-section">
-                        <BodyLong spacing>
-                            Jobbtreff er en ny tjeneste på arbeidsplassen.no. Nå kan du som er jobbsøker bli kjent med
-                            en bedrift som trenger folk. De forteller om hva slags kompetanse de trenger og om
-                            jobbmulighetene.
-                        </BodyLong>
-                        <BodyLong spacing>
-                            Dersom det er noe du gjerne vil at bedriften skal snakke om, kan du sende inn
-                            forhåndsdefinerte temaer før sendingen starter. Du kan også stille spørsmål underveis i
-                            sendingen.
-                        </BodyLong>
-                        <BodyLong>
-                            Hvis du synes det bedriften forteller om er interessant, kan du ta kontakt med dem etter
-                            jobbtreffet. De vil forklare hvordan du går fram.
-                        </BodyLong>
-                    </section>
+                    <BodyLong spacing>
+                        Jobbtreff er en ny tjeneste på arbeidsplassen.no. Nå kan du som er jobbsøker bli kjent med en
+                        bedrift som trenger folk. De forteller om hva slags kompetanse de trenger og om jobbmulighetene.
+                    </BodyLong>
+                    <BodyLong spacing>
+                        Dersom det er noe du gjerne vil at bedriften skal snakke om, kan du sende inn forhåndsdefinerte
+                        temaer før sendingen starter. Du kan også stille spørsmål underveis i sendingen.
+                    </BodyLong>
+                    <BodyLong className="mb-3">
+                        Hvis du synes det bedriften forteller om er interessant, kan du ta kontakt med dem etter
+                        jobbtreffet. De vil forklare hvordan du går fram.
+                    </BodyLong>
 
-                    <section className="arb-article-section">
-                        <Heading size="medium" level="2" spacing>
-                            Du er helt anonym
-                        </Heading>
-                        <BodyLong>
-                            Det er helt uforpliktende og ufarlig å delta. Verken bedriften eller de andre deltakerne
-                            ser, hører eller vet hvem som følger sendingen.
-                        </BodyLong>
-                    </section>
+                    <Heading size="large" level="2" spacing>
+                        Du er helt anonym
+                    </Heading>
+                    <BodyLong className="mb-3">
+                        Det er helt uforpliktende og ufarlig å delta. Verken bedriften eller de andre deltakerne ser,
+                        hører eller vet hvem som følger sendingen.
+                    </BodyLong>
 
-                    <section className="arb-article-section">
-                        <Heading size="medium" level="2" spacing>
-                            Slik følger du en sending
-                        </Heading>
-                        <BodyLong spacing>
-                            Sendingen blir annonsert på arbeidsplassen.no. På forhånd kan du lese hva bedriften har
-                            tenkt å fortelle om. Klikk på lenken for jobbtreffet når sendingen starter.
-                        </BodyLong>
-                        <BodyLong spacing>Du kan følge sendingen på telefon, PC eller Mac.</BodyLong>
-                        <BodyLong>Lykke til på jobbtreff!</BodyLong>
-                    </section>
+                    <Heading size="large" level="2" spacing>
+                        Slik følger du en sending
+                    </Heading>
+                    <BodyLong spacing>
+                        Sendingen blir annonsert på arbeidsplassen.no. På forhånd kan du lese hva bedriften har tenkt å
+                        fortelle om. Klikk på lenken for jobbtreffet når sendingen starter.
+                    </BodyLong>
+                    <BodyLong spacing>Du kan følge sendingen på telefon, PC eller Mac.</BodyLong>
+                    <BodyLong className="mb-3">Lykke til på jobbtreff!</BodyLong>
 
                     <section className="arb-article-section">
                         <LinkPanel
