3567ca8110e8f4156f351252a337a103a9e6fb7a
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-04-05 10:30:30 +0200

    workflow: add build.sh back

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+npm config set @navikt:registry https://npm.pkg.github.com
+npm config set //npm.pkg.github.com/:_authToken=$OPTIONAL_SECRET
+
+npm ci --prefer-offline --no-audit --ignore-scripts
+npm run lint
+npm run prettier:check
+npm run test
+
