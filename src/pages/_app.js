5a6a45fb039e55f267eb92d898830b83465f6cc8
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-04-13 15:31:57 +0200

    try initializing token

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,8 +5,10 @@ import "../common/styles/spacing.css";
 import "../common/styles/typography.css";
 import "../common/styles/article.css";
 import AuthenticationProvider from "@/src/common/contexts/AuthenticationProvider";
+import {initializeTokenX} from "@/src/middleware/tokenX";
 
 export default function App({ Component, pageProps }) {
+    initializeTokenX();
     return (
         <AuthenticationProvider>
             <Component {...pageProps} />
