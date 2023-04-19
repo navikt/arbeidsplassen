2f9f0204d1221ced8c3ba1bf0b13b0fd611d8150
otenav <ola.toft-eriksen@nav.no>
2023-04-19 12:56:01 +0200

    Antall stillinger

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,29 +2,33 @@ import MainPage from "@/src/common/components/MainPage";
 import Layout from "@/src/common/components/layout/Layout";
 import Head from "next/head";
 
-export async function getServerSideProps(context) {
+export async function getStaticProps() {
+    let positionCount;
     try {
         const response = await fetch("https://arbeidsplassen.nav.no/stillinger/api/search?from=0&size=1");
         const data = await response.json();
-        const positionCount = data.aggregations.positioncount.sum.value;
-
-        return {
-            props: {
-                positionCount: positionCount,
-            },
-        };
+        positionCount = data.aggregations.positioncount.sum.value;
     } catch (err) {
-        return { props: {} };
+        // ignore error
     }
+
+    return {
+        props: {
+            positionCount,
+            revalidated: new Date().getTime(),
+        },
+        revalidate: 60, // In seconds
+    };
 }
 
-export default function Page({ positionCount }) {
+export default function Page({ positionCount, revalidated }) {
     return (
         <Layout active="person">
             <Head>
                 <title>Arbeidsplassen - arbeidsplassen.no</title>
             </Head>
             <MainPage positionCount={positionCount} />
+            <div>{revalidated}</div>
         </Layout>
     );
 }
