318131b5aefc4f785b180126dcbe7c6864c48ba7
otenav <ola.toft-eriksen@nav.no>
2023-04-19 15:06:38 +0200

    Antall stillinger

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import Head from "next/head";
 
 export async function getStaticProps() {
     let positionCount;
+    let resp;
+    let code;
+    let error = null;
     try {
         const response = await fetch("http://pam-search-api/stillingsok/ad/_search?from=0&size=1", {
             method: "POST",
@@ -11,27 +14,38 @@ export async function getStaticProps() {
                 "Content-Type": "application/json",
             },
         });
+        resp = JSON.stringify(data);
+        code = response.status || response.statusCode;
         const data = await response.json();
         positionCount = data.aggregations.positioncount.sum.value;
     } catch (err) {
+        error = JSON.stringify(err);
         positionCount = null;
+        resp = null;
+        code = null;
     }
 
     return {
         props: {
             positionCount,
+            resp,
+            code,
+            error,
         },
         revalidate: 60, // In seconds
     };
 }
 
-export default function Page({ positionCount }) {
+export default function Page({ positionCount, code, resp, error }) {
     return (
         <Layout active="person">
             <Head>
                 <title>Arbeidsplassen - arbeidsplassen.no</title>
             </Head>
             <MainPage positionCount={positionCount} />
+            <p>{code}</p>
+            <p>{resp}</p>
+            <p>{error}</p>
         </Layout>
     );
 }
