4dee3c536a1f75ee60ce6dcfee674509e538dcb2
Audun Skjelvan <audun.skjelvan@nav.no>
2023-04-20 09:09:11 +0200

    adjust header

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' nav.no;
-  child-src nav.no;
-  style-src 'self' nav.no;
-  font-src 'self' nav.no;  
-`
-
 const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-  },
   {
     key: 'Strict-Transport-Security',
     value: 'max-age=63072000; includeSubDomains; preload'
