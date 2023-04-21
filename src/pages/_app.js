4b1e19b05b43168c71b769d77bde29e6558d8794
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-04-21 14:25:50 +0200

    test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import "@navikt/ds-css";
 import "@navikt/arbeidsplassen-css";
 import "../common/styles/index.css";
 import AuthenticationProvider from "@/src/common/contexts/AuthenticationProvider";
-import { initializeTokenX } from "@/src/middleware/tokenX";
 
 function App({ Component, pageProps }) {
     return (
@@ -11,16 +10,5 @@ function App({ Component, pageProps }) {
         </AuthenticationProvider>
     );
 }
-
-export const getServerSideProps = async ({ Component, ctx }) => {
-    await initializeTokenX(ctx.req, ctx.res, () => {});
-
-    // Call the getServerSideProps of the child component, if any
-    const pageProps = Component.getServerSideProps
-        ? await Component.getServerSideProps(ctx)
-        : {};
-
-    return { props: { pageProps } };
-};
 
 export default App;
--- src/pages/api/isAuthenticated.js
+++ src/pages/api/isAuthenticated.js
@@ -1,6 +1,86 @@
-import {tokenIsValid} from "@/src/middleware/tokenX";
+import { createRemoteJWKSet, jwtVerify } from 'jose';
+import { Issuer } from "openid-client";
+
+let initialized = false;
+let tokenXIssuer;
+let idPortenIssuer;
+let tokenXClient;
+let remoteJWKSet;
+
+async function performSetup() {
+    try {
+        tokenXIssuer = await Issuer.discover(process.env.TOKEN_X_WELL_KNOWN_URL);
+        idPortenIssuer = await Issuer.discover(process.env.IDPORTEN_WELL_KNOWN_URL);
+        tokenXClient = new tokenXIssuer.Client(
+            {
+                client_id: process.env.TOKEN_X_CLIENT_ID,
+                token_endpoint_auth_method: 'private_key_jwt',
+                token_endpoint_auth_signing_alg: "RS256"
+            },
+            {
+                keys: [JSON.parse(process.env.TOKEN_X_PRIVATE_JWK)],
+            }
+        );
+        const jwksUrl = new URL(process.env.IDPORTEN_JWKS_URI);
+        remoteJWKSet = createRemoteJWKSet(jwksUrl);
+
+        // success
+        initialized = true;
+    } catch (e) {
+        throw new Error(`Could not initialize TokenX: ${e}`);
+        initialized = false;
+    }
+}
+
+async function tokenIsValid(token) {
+    try {
+        const verification = await jwtVerify(token, remoteJWKSet, {
+            audience: process.env.IDPORTEN_AUDIENCE,
+            issuer: idPortenIssuer.metadata.issuer,
+        });
+
+        return !!verification.payload;
+    } catch (e) {
+        console.error(`An error occurred during token validation: ${e.message}`);
+        return false;
+    }
+}
+
+async function getTokenX(token, audience) {
+    let tokenX;
+    const additionalClaims = {
+        clientAssertionPayload: {
+            aud: tokenXClient.issuer.metadata.token_endpoint,
+            nbf: Math.floor(Date.now() / 1000),
+        }
+    }
+
+    try {
+        tokenX = await tokenXClient.grant(
+            {
+                grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
+                client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+                subject_token_type: "urn:ietf:params:oauth:token-type:jwt",
+                audience: audience,
+                subject_token: token
+            },
+            additionalClaims
+        );
+    } catch (e) {
+        console.error(`Could not exchange to tokenX: ${e.message}`);
+    }
+    return tokenX;
+}
 
 export default async function handler(req, res) {
+    if (!initialized)
+        try {
+            await performSetup();
+        } catch (e) {
+            res.status(500).send("Could not perform setup");
+            return;
+        }
+
     if (req.headers.authorization) {
         const accessToken = req.headers.authorization.split(" ")[1];
         const validToken = await tokenIsValid(accessToken);
@@ -10,6 +90,6 @@ export default async function handler(req, res) {
             res.status(401).send("Unauthorized");
         }
     } else {
-        res.status(401).send("Error: could not authorize");
+        res.status(401).send("Could not authorize");
     }
 }
