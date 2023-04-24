5857215529fc4ac0a7ad3c4765856ab93698431f
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-04-24 15:34:35 +0200

    clean up

--- src/pages/api/isAuthenticated.js
+++ src/pages/api/isAuthenticated.js
@@ -3,13 +3,17 @@ import { validateIdportenToken } from '@navikt/next-auth-wonderwall'
 export default async function handler(req, res) {
     const bearerToken = req.headers.authorization;
     if (bearerToken) {
-        const validToken = await validateIdportenToken(bearerToken);
-        if (validToken) {
-            res.status(200).send("OK");
-        } else {
-            res.status(401).send("Unauthorized");
+        try {
+            const validToken = await validateIdportenToken(bearerToken);
+            if (validToken) {
+                res.status(200).send("OK");
+            } else {
+                res.status(401).send("Unauthorized");
+            }
+        } catch (e) {
+            res.status(500).send("Could not authorize");
         }
     } else {
-        res.status(401).send("Could not authorize");
+        res.status(401).send("No token found");
     }
 }
