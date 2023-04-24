31d33f980c3fb1866f8272dfad98996462f11b72
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-04-24 13:40:54 +0200

    use next-auth-wonderwall

--- src/pages/api/isAuthenticated.js
+++ src/pages/api/isAuthenticated.js
@@ -1,86 +1,9 @@
-import { createRemoteJWKSet, jwtVerify } from 'jose';
-import { Issuer } from "openid-client";
-
-let initialized = false;
-let tokenXIssuer;
-let idPortenIssuer;
-let tokenXClient;
-let remoteJWKSet;
-
-async function performSetup() {
-    if (initialized)
-        return;
-
-    try {
-        tokenXIssuer = await Issuer.discover(process.env.TOKEN_X_WELL_KNOWN_URL);
-        idPortenIssuer = await Issuer.discover(process.env.IDPORTEN_WELL_KNOWN_URL);
-        tokenXClient = new tokenXIssuer.Client(
-            {
-                client_id: process.env.TOKEN_X_CLIENT_ID,
-                token_endpoint_auth_method: 'private_key_jwt',
-                token_endpoint_auth_signing_alg: "RS256"
-            },
-            {
-                keys: [JSON.parse(process.env.TOKEN_X_PRIVATE_JWK)],
-            }
-        );
-        const jwksUrl = new URL(process.env.IDPORTEN_JWKS_URI);
-        remoteJWKSet = createRemoteJWKSet(jwksUrl);
-
-        // success
-        initialized = true;
-    } catch (e) {
-        throw new Error(`Could not initialize TokenX: ${e}`);
-        initialized = false;
-    }
-}
-
-async function tokenIsValid(token) {
-    try {
-        const verification = await jwtVerify(token, remoteJWKSet, {
-            audience: process.env.IDPORTEN_AUDIENCE,
-            issuer: idPortenIssuer.metadata.issuer,
-        });
-
-        return !!verification.payload;
-    } catch (e) {
-        throw new Error(`An error occurred during token validation: ${e.message}`);
-        return false;
-    }
-}
-
-async function getTokenX(token, audience) {
-    let tokenX;
-    const additionalClaims = {
-        clientAssertionPayload: {
-            aud: tokenXClient.issuer.metadata.token_endpoint,
-            nbf: Math.floor(Date.now() / 1000),
-        }
-    }
-
-    try {
-        tokenX = await tokenXClient.grant(
-            {
-                grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
-                client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-                subject_token_type: "urn:ietf:params:oauth:token-type:jwt",
-                audience: audience,
-                subject_token: token
-            },
-            additionalClaims
-        );
-    } catch (e) {
-        throw new Error(`Could not exchange to tokenX: ${e.message}`);
-    }
-    return tokenX;
-}
+import { validateIdportenToken } from '@navikt/next-auth-wonderwall'
 
 export default async function handler(req, res) {
-    await performSetup();
-
-    if (req.headers.authorization) {
-        const accessToken = req.headers.authorization.split(" ")[1];
-        const validToken = await tokenIsValid(accessToken);
+    const bearerToken = req.headers.authorization;
+    if (bearerToken) {
+        const validToken = await validateIdportenToken(bearerToken);
         if (validToken) {
             res.status(200).send("OK");
         } else {
