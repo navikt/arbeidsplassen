07ec367cc0f4e732d372940a2febac54f16acd5e
otenav <ola.toft-eriksen@nav.no>
2023-04-24 13:46:26 +0200

    Forsøker å hente antall stillinger - rollback

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,6 @@ import MainPage from "@/src/common/components/MainPage";
 import Layout from "@/src/common/components/layout/Layout";
 import Head from "next/head";
 
-export async function getStaticProps() {
-    let positionCount;
-    try {
-        const response = await fetch("https://arbeidsplassen.nav.no/stillinger/api/search?from=0&size=1");
-        const data = await response.json();
-        positionCount = data.aggregations.positioncount.sum.value;
-    } catch (err) {
-        positionCount = null;
-    }
-
-    return {
-        props: {
-            positionCount,
-        },
-        revalidate: 60, // In seconds
-    };
-}
-
 export default function Page({ positionCount }) {
     return (
         <Layout active="person">
