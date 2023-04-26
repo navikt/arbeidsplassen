fcce00454b0f14a960541a7f009c3671efb2a3e9
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-04-26 11:12:06 +0200

    add page for successful verification of email

--- src/pages/epost-verifisering-utgaatt.js
+++ src/pages/epost-verifisering-utgaatt.js
@@ -18,7 +18,9 @@ export default function epostVerifiseringUtgaatt() {
                         d="M12 10.6512L15.3719 7.27934C15.7444 6.90689 16.3482 6.90689 16.7207 7.27934C17.0931 7.65179 17.0931 8.25565 16.7207 8.6281L13.3488 12L16.7207 15.3719C17.0931 15.7444 17.0931 16.3482 16.7207 16.7207C16.3482 17.0931 15.7444 17.0931 15.3719 16.7207L12 13.3488L8.6281 16.7207C8.25565 17.0931 7.65179 17.0931 7.27934 16.7207C6.90689 16.3482 6.90689 15.7444 7.27934 15.3719L10.6512 12L7.27934 8.6281C6.90689 8.25565 6.90689 7.65179 7.27934 7.27934C7.65179 6.90689 8.25565 6.90689 8.6281 7.27934L12 10.6512Z"
                         fill="white"/>
                 </svg>
-                <Heading spacing size="xlarge" level="1">Feil, lenken er utgått</Heading>
+                <Heading spacing size="xlarge" level="1">
+                    Feil, lenken er utgått
+                </Heading>
                 <Ingress spacing>
                     Du kan bestille e-post med ny lenke under <Link href="/personinnstillinger">innstillinger</Link>
                 </Ingress>
