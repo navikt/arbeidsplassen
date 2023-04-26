7e6bc6b97aa2941855441c9bbe1f1b9fcfa0becf
Audun Skjelvan <audun.skjelvan@nav.no>
2023-04-26 11:54:16 +0200

    headers

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const ContentSecurityPolicy = `
   default-src 'self';
   img-src 'self' data:;
   script-src 'self' 'unsafe-eval';
-  style-src 'unsafe-inline' fonts.googleapis.com;
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com nav.no;
   font-src 'self' fonts.googleapis.com fonts.gstatic.com nav.no;
 `
 
