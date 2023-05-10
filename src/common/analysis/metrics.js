c8332b00975eed2a6ddae1924e207d41ebaf0b0e
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-05-10 14:23:27 +0200

    initial commit

---
+++ src/common/analysis/metrics.js
@@ -0,0 +1,26 @@
+import * as amplitude from '@amplitude/analytics-browser';
+
+function setUpAmplitude(apiKey) {
+    try {
+        amplitude.init(apiKey, undefined, {
+            serverUrl: `https://amplitude.nav.no/collect`,
+            transport: amplitude.Types.TransportType.SendBeacon,
+            defaultTracking: {
+                pageViews: true,
+                sessions: true,
+                formInteractions: true,
+            },
+            /** Need this for /collect-auto according to https://nav-it.slack.com/archives/CMK1SCBP1/p1669722646425599
+             * but seems to work fine with /collect? Keeping it here just in case.
+            IngestionMetadata: {
+                sourceName: window.location.toString(),
+            },
+            */
+        });
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+export default setUpAmplitude;
