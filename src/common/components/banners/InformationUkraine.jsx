079e9c940ee63500ff9cf9112acdba9f2bc8ed73
Marthe <marthe.hagen@nav.no>
2023-05-11 09:47:35 +0200

    Fiks størrelse på banner

--- src/common/components/banners/InformationUkraine.jsx
+++ src/common/components/banners/InformationUkraine.jsx
@@ -4,7 +4,7 @@ import UkrainianFlag from "@/src/common/components/images/UkrainianFlag";
 
 export default function InformationUkraine() {
     return (
-        <Panel className="container-large arb-tertiary-bg-text text-center">
+        <Panel className="container-xlarge arb-tertiary-bg-text text-center">
             <Heading spacing level="2" size="large">
                 Information about working in Norway for refugees from Ukraine
                 <UkrainianFlag className="h-2" ariaHidden="Ukrainsk flagg" />
