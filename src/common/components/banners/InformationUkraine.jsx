c3b820e3ffe9fca49f5249114465c9881f3df9ee
otenav <ola.toft-eriksen@nav.no>
2023-05-22 08:35:27 +0200

    Setter språk på lenker

--- src/common/components/banners/InformationUkraine.jsx
+++ src/common/components/banners/InformationUkraine.jsx
@@ -5,27 +5,27 @@ import UkrainianFlag from "@/src/common/components/images/UkrainianFlag";
 export default function InformationUkraine() {
     return (
         <Panel className="container-xlarge arb-tertiary-bg-text text-center">
-            <Heading spacing level="2" size="large">
+            <Heading spacing level="2" size="large" lang="en">
                 Information about working in Norway for refugees from Ukraine
-                <UkrainianFlag className="h-2" ariaHidden="Ukrainsk flagg" />
+                <UkrainianFlag className="h-2" ariaHidden="true" />
             </Heading>
             <ul className="ukraine-grid">
                 <li>
-                    <BodyLong>
+                    <BodyLong lang="en">
                         <NextLink href="/en/work-in-norway" passHref legacyBehavior>
                             <DsLink>Information about working in Norway</DsLink>
                         </NextLink>
                     </BodyLong>
                 </li>
                 <li>
-                    <BodyLong>
+                    <BodyLong lang="ua">
                         <NextLink href="/ua/work-in-norway" passHref legacyBehavior>
                             <DsLink>Інформація українською мовою</DsLink>
                         </NextLink>
                     </BodyLong>
                 </li>
                 <li>
-                    <BodyLong className="text-center">
+                    <BodyLong className="text-center" lang="ru">
                         <NextLink href="/ru/work-in-norway" passHref legacyBehavior>
                             <DsLink>Информация на русском языке</DsLink>
                         </NextLink>
