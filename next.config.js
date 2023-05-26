a5b1b2b15f0d702cdcb508116334b011615da6f9
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-05-26 10:40:28 +0200

    remove cdn.nav.no from font-src

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const ContentSecurityPolicy = `
   img-src 'self' data:;
   script-src 'self' 'unsafe-eval';
   style-src 'self' 'unsafe-inline' fonts.googleapis.com nav.no;
-  font-src 'self' fonts.googleapis.com fonts.gstatic.com nav.no cdn.nav.no;
+  font-src 'self' fonts.googleapis.com fonts.gstatic.com nav.no;
   connect-src 'self' amplitude.nav.no;
 `;
 
