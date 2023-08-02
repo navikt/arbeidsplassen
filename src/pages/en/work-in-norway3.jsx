59373f6ec2588ed084ef31e918aa7c624cfcc8a0
Minh Ha Do <minh.ha.do@nav.no>
2023-08-02 14:07:22 +0200

    Add links

--- src/pages/en/work-in-norway3.jsx
+++ src/pages/en/work-in-norway3.jsx
@@ -2,6 +2,7 @@ import { BodyLong, Heading, BodyShort, LinkPanel, Link as DsLink } from "@navikt
 import Layout from "@/src/common/components/layout/Layout";
 import Head from "next/head";
 import TableOfContents from "@/src/common/components/TableOfContents";
+import NextLink from "next/link";
 
 export default function workInNorwayEn() {
     return (
@@ -25,8 +26,11 @@ export default function workInNorwayEn() {
                             When can I start looking for a job?
                         </Heading>
                         <BodyLong spacing>
-                            You must have been granted <DsLink>collective protection</DsLink> before you can start
-                            searching for and applying for jobs.
+                            You must have been granted{" "}
+                            <DsLink href="https://udi.no/en/information-ukraine-and-russia/situation-in-ukraine/stay-in-norway/protection-asylum-in-norway/">
+                                collective protection
+                            </DsLink>{" "}
+                            before you can start searching for and applying for jobs.
                         </BodyLong>
                         <BodyLong spacing>
                             You cannot apply for jobs or work while The Norwegian Directorate of Immigration (UDI)
@@ -36,17 +40,26 @@ export default function workInNorwayEn() {
                             Relevant news and articles
                         </Heading>
                         <div className="arb-link-panel-grid mb-3">
-                            <LinkPanel className="arb-tertiary-bg-text arb-link-panel" href="/cv">
+                            <LinkPanel
+                                className="arb-tertiary-bg-text arb-link-panel"
+                                href="https://www.udi.no/en/information-ukraine-and-russia/situation-in-ukraine/stay-in-norway/frequently-asked-questions-and-answers/"
+                            >
                                 <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
                                     Questions and answers around collective protection
                                 </LinkPanel.Title>
                             </LinkPanel>
-                            <LinkPanel className="arb-tertiary-bg-text arb-link-panel" href="/cv">
+                            <LinkPanel
+                                className="arb-tertiary-bg-text arb-link-panel"
+                                href="https://www.udi.no/en/information-ukraine-and-russia/situation-in-ukraine/stay-in-norway/protection-asylum-in-norway/"
+                            >
                                 <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
                                     Protection for those who have fled Ukraine
                                 </LinkPanel.Title>
                             </LinkPanel>
-                            <LinkPanel className="arb-tertiary-bg-text arb-link-panel" href="/cv">
+                            <LinkPanel
+                                className="arb-tertiary-bg-text arb-link-panel"
+                                href="https://www.udi.no/en/information-ukraine-and-russia/situation-in-ukraine/stay-in-norway/frequently-asked-questions-and-answers/#link-30134"
+                            >
                                 <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
                                     Are you intending to work in Norway?
                                 </LinkPanel.Title>
@@ -56,26 +69,33 @@ export default function workInNorwayEn() {
                             Where can I find a job?
                         </Heading>
                         <BodyLong spacing>
-                            You can look for a job at arbeidsplassen.no. This is{" "}
-                            <DsLink>one of the largest database for job advertisements in Norway.</DsLink> Here you can
-                            also find <DsLink>job advertisements in english.</DsLink>
+                            You can look for a job at arbeidsplassen.no, one of the largest databases for job ads in
+                            Norway. Here you can also find{" "}
+                            <DsLink href="/stillinger?q=english">job advertisements in english.</DsLink>
                         </BodyLong>
                         <BodyLong spacing>
-                            With <DsLink>"Jobbtreff" (norwegian language)</DsLink> at arbeidsplassen.no you can follow a
-                            live broadcast where companies present themselves and the positions you can apply for.
-                            Jobbtreff is mainly held in norwegian. Take a look at{" "}
-                            <DsLink>"Se kommende jobbtreff"</DsLink> if you can find someone in english.
+                            With{" "}
+                            <NextLink href="/mot-bedrifter-pa-en-ny-mate" passHref legacyBehavior>
+                                <DsLink>"Jobbtreff" (norwegian language)</DsLink>
+                            </NextLink>{" "}
+                            at arbeidsplassen.no you can follow a live broadcast where companies present themselves and
+                            the positions you can apply for. Jobbtreff is mainly held in norwegian. Take a look at{" "}
+                            <DsLink href="/jobbtreff">"Se kommende jobbtreff"</DsLink> if you can find someone in
+                            english.
                         </BodyLong>
 
                         <Heading id="who-can-i-contact" size="large" level="2" spacing>
                             Who can I contact for assistance and guidance?
                         </Heading>
                         <BodyLong spacing>
-                            If you need assistance finding a job, you can <DsLink>contact NAV</DsLink> for information
-                            and guidance. If you are registered as a job seeker at NAV, NAV can help you to get in touch
+                            If you need assistance finding a job, you can{" "}
+                            <DsLink href="https://www.nav.no/kontaktoss/en">contact NAV</DsLink> for information and
+                            guidance. If you are registered as a job seeker at NAV, NAV can help you to get in touch
                             with relevant employers and provide you with various measures in order to get a job. You can{" "}
-                            <DsLink>register as a job seeker at nav.no.</DsLink> If you need assistance you can contact
-                            your local NAV office.
+                            <DsLink href="https://www.nav.no/arbeid/registrering">
+                                register as a job seeker at nav.no. (norwegian language)
+                            </DsLink>{" "}
+                            If you need assistance you can contact your local NAV office.
                         </BodyLong>
                         <BodyLong spacing>You have the right to get an interpreter when contacting NAV.</BodyLong>
                         <BodyLong spacing>If you have questions, please call NAV at 55 55 33 33 </BodyLong>
@@ -86,12 +106,12 @@ export default function workInNorwayEn() {
                         <div className="arb-link-panel-grid link-panel-stack mb-3">
                             <LinkPanel className="arb-tertiary-bg-text arb-link-panel" href="/cv">
                                 <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
-                                    Find your local NAV office Optional description
+                                    Find your local NAV office
                                 </LinkPanel.Title>
                             </LinkPanel>
                             <LinkPanel className="arb-tertiary-bg-text arb-link-panel" href="/cv">
                                 <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
-                                    More information about searching for a job (norwegian language) Optional description
+                                    More information about searching for a job (norwegian language)
                                 </LinkPanel.Title>
                             </LinkPanel>
                         </div>
@@ -101,8 +121,9 @@ export default function workInNorwayEn() {
                         </Heading>
                         <BodyLong spacing>
                             Employers might set up language requirements through the job advertisement. If you can
-                            communicate in basic english, you can look for <DsLink>job advertisement in english</DsLink>{" "}
-                            or where english is specified as working language.
+                            communicate in basic english, you can look for{" "}
+                            <DsLink href="/stillinger?q=english">job advertisement in english</DsLink> or where english
+                            is specified as working language.
                         </BodyLong>
 
                         <Heading size="xsmall" level="3" spacing>
@@ -110,12 +131,16 @@ export default function workInNorwayEn() {
                         </Heading>
                         <BodyLong spacing>
                             Persons with granted collective protection may be entitled to free norwegian courses.
-                            Further <DsLink>information about the Introduction Programme</DsLink> and your rights to
-                            attend free norwegian courses are to be found at the official website of IMDi.
+                            Further{" "}
+                            <DsLink href="https://www.imdi.no/globalassets/dokumenter/informasjon-om-introduksjonsprogram-og-sprakopplaring-for-personer-med-kollektiv-beskyttelse/informasjon-om-introduksjonsprogram-2022---engelsk.pdf">
+                                information about the Introduction Programme
+                            </DsLink>{" "}
+                            and your rights to attend free norwegian courses are to be found at the official website of
+                            IMDi.
                         </BodyLong>
                         <BodyLong spacing>
-                            There are also possibilities to learn norwegian language skills while working through the{" "}
-                            <DsLink>Flexible fast track.</DsLink>
+                            There are also possibilities to learn norwegian language skills while working through the
+                            flexible fast track.
                         </BodyLong>
 
                         <Heading size="small" level="3" spacing>
@@ -124,8 +149,7 @@ export default function workInNorwayEn() {
                         <div className="arb-link-panel-grid link-panel-stack mb-3">
                             <LinkPanel className="arb-tertiary-bg-text arb-link-panel" href="/cv">
                                 <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
-                                    Information about the Introduction Programme and language instruction Optional
-                                    description
+                                    Information about the Introduction Programme and language instruction
                                 </LinkPanel.Title>
                             </LinkPanel>
                         </div>
