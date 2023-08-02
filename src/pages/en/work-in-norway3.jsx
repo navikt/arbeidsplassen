69a77dba00051697eba757322ff95bf942b5e422
Minh Ha Do <minh.ha.do@nav.no>
2023-08-02 11:42:24 +0200

    Add links, text formatting

--- src/pages/en/work-in-norway3.jsx
+++ src/pages/en/work-in-norway3.jsx
@@ -1,4 +1,4 @@
-import { BodyLong, Heading, BodyShort, LinkPanel } from "@navikt/ds-react";
+import { BodyLong, Heading, BodyShort, LinkPanel, Link as DsLink } from "@navikt/ds-react";
 import Layout from "@/src/common/components/layout/Layout";
 import Head from "next/head";
 import TableOfContents from "@/src/common/components/TableOfContents";
@@ -21,18 +21,21 @@ export default function workInNorwayEn() {
                 <div className="flex">
                     <TableOfContents selectorPrefix="main" />
                     <article className="container-small page-margin-top-and-bottom ukraine-page" lang="en">
-                        <Heading id="when" size="large" level="2" spacing>
+                        <Heading id="when-can-i-start-looking" size="large" level="2" spacing>
                             When can I start looking for a job?
                         </Heading>
                         <BodyLong spacing>
-                            You must have been granted collective protection before you can start searching for and
-                            applying for jobs.. You cannot apply for jobs or work while The Norwegian Directorate of
-                            Immigration (UDI) processes your application.
+                            You must have been granted <DsLink>collective protection</DsLink> before you can start
+                            searching for and applying for jobs.
+                        </BodyLong>
+                        <BodyLong spacing>
+                            You cannot apply for jobs or work while The Norwegian Directorate of Immigration (UDI)
+                            processes your application.
                         </BodyLong>
                         <Heading size="small" level="3" spacing>
                             Relevant news and articles
                         </Heading>
-                        <div className="arb-link-panel-grid mb-5">
+                        <div className="arb-link-panel-grid mb-3">
                             <LinkPanel className="arb-tertiary-bg-text arb-link-panel" href="/cv">
                                 <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
                                     Questions and answers around collective protection
@@ -49,53 +52,83 @@ export default function workInNorwayEn() {
                                 </LinkPanel.Title>
                             </LinkPanel>
                         </div>
-                        <Heading id="where-find" size="large" level="2" spacing>
+                        <Heading id="where-can-i-find" size="large" level="2" spacing>
                             Where can I find a job?
                         </Heading>
                         <BodyLong spacing>
-                            You can look for a job at arbeidsplassen.no. This is one of the largest database for job
-                            advertisements in Norway. Here you can also find job advertisements in english.
+                            You can look for a job at arbeidsplassen.no. This is{" "}
+                            <DsLink>one of the largest database for job advertisements in Norway.</DsLink> Here you can
+                            also find <DsLink>job advertisements in english.</DsLink>
                         </BodyLong>
                         <BodyLong spacing>
-                            With “Jobbtreff “ (norwegian language) at arbeidsplassen.no you can follow a live broadcast
-                            where companies present themselves and the positions you can apply for. Jobbtreff is mainly
-                            held in norwegian. Take a look at “Se kommende jobbtreff” if you can find someone in
-                            english.
+                            With <DsLink>"Jobbtreff" (norwegian language)</DsLink> at arbeidsplassen.no you can follow a
+                            live broadcast where companies present themselves and the positions you can apply for.
+                            Jobbtreff is mainly held in norwegian. Take a look at{" "}
+                            <DsLink>"Se kommende jobbtreff"</DsLink> if you can find someone in english.
                         </BodyLong>
 
-                        <Heading id="who" size="large" level="2" spacing>
+                        <Heading id="who-can-i-contact" size="large" level="2" spacing>
                             Who can I contact for assistance and guidance?
                         </Heading>
                         <BodyLong spacing>
-                            If you need assistance finding a job, you can contact NAV for information and guidance. If
-                            you are registered as a job seeker at NAV, NAV can help you to get in touch with relevant
-                            employers and provide you with various measures in order to get a job. You can register as a
-                            job seeker at nav.no. If you need assistance you can contact your local NAV office.
+                            If you need assistance finding a job, you can <DsLink>contact NAV</DsLink> for information
+                            and guidance. If you are registered as a job seeker at NAV, NAV can help you to get in touch
+                            with relevant employers and provide you with various measures in order to get a job. You can{" "}
+                            <DsLink>register as a job seeker at nav.no.</DsLink> If you need assistance you can contact
+                            your local NAV office.
                         </BodyLong>
-                        <BodyLong spacing>You have the right to get an interpreter. when contacting NAV.</BodyLong>
-                        <BodyLong spacing>If you have questions, please call NAV at 55 55 33 33</BodyLong>
+                        <BodyLong spacing>You have the right to get an interpreter when contacting NAV.</BodyLong>
+                        <BodyLong spacing>If you have questions, please call NAV at 55 55 33 33 </BodyLong>
 
-                        <Heading id="where-work" size="large" level="2" spacing>
+                        <Heading size="small" level="3" spacing>
+                            Relevant news and articles
+                        </Heading>
+                        <div className="arb-link-panel-grid link-panel-stack mb-3">
+                            <LinkPanel className="arb-tertiary-bg-text arb-link-panel" href="/cv">
+                                <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
+                                    Find your local NAV office Optional description
+                                </LinkPanel.Title>
+                            </LinkPanel>
+                            <LinkPanel className="arb-tertiary-bg-text arb-link-panel" href="/cv">
+                                <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
+                                    More information about searching for a job (norwegian language) Optional description
+                                </LinkPanel.Title>
+                            </LinkPanel>
+                        </div>
+
+                        <Heading id="where-can-i-work" size="large" level="2" spacing>
                             Where can I work without norwegian language skills?
                         </Heading>
                         <BodyLong spacing>
                             Employers might set up language requirements through the job advertisement. If you can
-                            communicate in basic english, you can look for job advertisement in english or where english
-                            is specified as working language.
+                            communicate in basic english, you can look for <DsLink>job advertisement in english</DsLink>{" "}
+                            or where english is specified as working language.
                         </BodyLong>
 
-                        <Heading size="small" level="3" spacing>
+                        <Heading size="xsmall" level="3" spacing>
                             Norwegian courses
                         </Heading>
                         <BodyLong spacing>
                             Persons with granted collective protection may be entitled to free norwegian courses.
-                            Further information about the Introduction Programme and your rights to attend free
-                            norwegian courses are to be found at the official website of IMDi.
+                            Further <DsLink>information about the Introduction Programme</DsLink> and your rights to
+                            attend free norwegian courses are to be found at the official website of IMDi.
                         </BodyLong>
-                        <BodyLong>
-                            There are also possibilities to learn norwegian language skills while working through the
-                            Flexible fast track.
+                        <BodyLong spacing>
+                            There are also possibilities to learn norwegian language skills while working through the{" "}
+                            <DsLink>Flexible fast track.</DsLink>
                         </BodyLong>
+
+                        <Heading size="small" level="3" spacing>
+                            Relevant news and articles
+                        </Heading>
+                        <div className="arb-link-panel-grid link-panel-stack mb-3">
+                            <LinkPanel className="arb-tertiary-bg-text arb-link-panel" href="/cv">
+                                <LinkPanel.Title className="navds-link-panel__title navds-heading--small">
+                                    Information about the Introduction Programme and language instruction Optional
+                                    description
+                                </LinkPanel.Title>
+                            </LinkPanel>
+                        </div>
                     </article>
                 </div>
             </div>
