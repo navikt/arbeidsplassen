81c0e20191fb54cebb7b2eb5713e430888014866
Minh Ha Do <minh.ha.do@nav.no>
2023-08-02 09:39:12 +0200

    Eslint fixes, remove h3 from table

--- src/common/components/TableOfContents.jsx
+++ src/common/components/TableOfContents.jsx
@@ -1,19 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
+import { Label } from "@navikt/ds-react";
+
+// https://www.emgoto.com/react-table-of-contents/
 
 const getNestedHeadings = (headingElements) => {
     const nestedHeadings = [];
 
-    headingElements.forEach((heading, index) => {
+    headingElements.forEach((heading) => {
         const { innerText: title, id } = heading;
 
         if (heading.nodeName === "H2") {
             nestedHeadings.push({ id, title, items: [] });
-        } else if (heading.nodeName === "H3" && nestedHeadings.length > 0) {
-            nestedHeadings[nestedHeadings.length - 1].items.push({
-                id,
-                title,
-            });
         }
     });
 
@@ -24,7 +22,7 @@ const useHeadingsData = (prefix = "") => {
     const [nestedHeadings, setNestedHeadings] = useState([]);
 
     useEffect(() => {
-        const headingElements = Array.from(document.querySelectorAll(`${prefix} h2, ${prefix} h3`));
+        const headingElements = Array.from(document.querySelectorAll(`${prefix} h2`));
 
         const newNestedHeadings = getNestedHeadings(headingElements);
         setNestedHeadings(newNestedHeadings);
@@ -49,25 +47,6 @@ function Headings({ headings, activeId }) {
                     >
                         {heading.title}
                     </a>
-                    {heading.items.length > 0 && (
-                        <ul>
-                            {heading.items.map((child) => (
-                                <li key={child.id} className={child.id === activeId ? "active" : ""}>
-                                    <a
-                                        href={`#${child.id}`}
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            document.querySelector(`#${child.id}`).scrollIntoView({
-                                                behavior: "smooth",
-                                            });
-                                        }}
-                                    >
-                                        {child.title}
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
                 </li>
             ))}
         </ul>
@@ -84,20 +63,21 @@ Headings.propTypes = {
 const useIntersectionObserver = (setActiveId, prefix = "") => {
     const headingElementsRef = useRef({});
     useEffect(() => {
+        const headingElements = Array.from(document.querySelectorAll(`${prefix} h2`));
         const callback = (headings) => {
             headingElementsRef.current = headings.reduce((map, headingElement) => {
-                map[headingElement.target.id] = headingElement;
-                return map;
+                const mapping = map;
+                mapping[headingElement.target.id] = headingElement;
+                return mapping;
             }, headingElementsRef.current);
-
             const visibleHeadings = [];
+
             Object.keys(headingElementsRef.current).forEach((key) => {
                 const headingElement = headingElementsRef.current[key];
                 if (headingElement.isIntersecting) visibleHeadings.push(headingElement);
             });
 
             const getIndexFromId = (id) => headingElements.findIndex((heading) => heading.id === id);
-
             if (visibleHeadings.length === 1) {
                 setActiveId(visibleHeadings[0].target.id);
             } else if (visibleHeadings.length > 1) {
@@ -112,8 +92,6 @@ const useIntersectionObserver = (setActiveId, prefix = "") => {
             rootMargin: "0px 0px -40% 0px",
         });
 
-        const headingElements = Array.from(document.querySelectorAll(`${prefix} h2, ${prefix} h3`));
-
         headingElements.forEach((element) => observer.observe(element));
 
         return () => observer.disconnect();
@@ -127,6 +105,7 @@ function TableOfContents({ selectorPrefix }) {
 
     return (
         <div className="table-of-contents-wrapper">
+            <Label>Page contents</Label>
             <nav className="table-of-contents" aria-label="Table of contents">
                 <Headings headings={nestedHeadings} activeId={activeId} />
             </nav>
