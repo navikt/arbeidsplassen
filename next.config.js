0cccb306200eb37b07a09ce90f525dd40919f02e
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-09-22 09:03:28 +0200

    fix content security policy 2

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const ContentSecurityPolicy = `
   script-src 'self' 'unsafe-eval';
   style-src 'self' 'unsafe-inline' fonts.googleapis.com nav.no;
   font-src 'self' fonts.googleapis.com fonts.gstatic.com nav.no;
-  connect-src 'self' amplitude.nav.no ${process.env.TELEMTRY_URL};
+  connect-src 'self' amplitude.nav.no telemetry*.nav.no/collect;
 `;
 
 const securityHeaders = [
