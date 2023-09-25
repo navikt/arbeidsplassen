bfee78539c1b7b5638e46a13c587d674e3f0556f
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-09-25 09:13:06 +0200

    add instrumentations and a log line test

--- src/common/analysis/faro.js
+++ src/common/analysis/faro.js
@@ -1,4 +1,5 @@
-import { initializeFaro } from "@grafana/faro-web-sdk";
+import { getWebInstrumentations, initializeFaro } from "@grafana/faro-web-sdk";
+import { TracingInstrumentation } from "@grafana/faro-web-tracing";
 
 export default function setupFaro() {
     console.log("appVersion: ", window.publicEnv ? window.publicEnv.APP_VERSION : "");
@@ -8,6 +9,13 @@ export default function setupFaro() {
         app: {
             name: "arbeidsplassen", // required
             version: window.publicEnv ? window.publicEnv.APP_VERSION : "", // optional; useful in Grafana to get diff between versions
+            instrumentations: [
+                ...getWebInstrumentations({
+                    captureConsole: true,
+                }),
+                new TracingInstrumentation(),
+            ],
         },
     });
+    console.log("Faro Initialized");
 }
