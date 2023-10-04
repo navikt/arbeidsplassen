6b01457526e971027505e0d44653d5bf4adc81f5
otenav <ola.toft-eriksen@nav.no>
2023-10-04 09:54:04 +0200

    Use NotFound component from arbeidsplassen design system

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,6 @@
-import { BodyLong, Heading } from "@navikt/ds-react";
 import Layout from "@/src/common/components/layout/Layout";
 import Head from "next/head";
-import ConfusedGrape from "@/src/common/components/images/ConfusedGrape";
+import { NotFound } from "@navikt/arbeidsplassen-react";
 
 export default function Custom404() {
     return (
@@ -9,14 +8,8 @@ export default function Custom404() {
             <Head>
                 <title>Fant ikke siden - arbeidsplassen.no</title>
             </Head>
-            <div className="container-small page-margin-top-and-bottom text-center">
-                <ConfusedGrape className="mb-8" ariaHidden="true" />
-                <Heading spacing size="large" level="1">
-                    Fant ikke siden
-                </Heading>
-                <BodyLong>
-                    Nettsiden kan være slettet eller flyttet, eller det var en feil i lenken som førte deg hit.
-                </BodyLong>
+            <div className="container-small page-margin-top-and-bottom">
+                <NotFound />
             </div>
         </Layout>
     );
