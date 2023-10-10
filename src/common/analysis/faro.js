d8dd22d06eae13f702efc53d7e335dadce7222ce
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-10-10 14:20:38 +0200

    disable faro for dev environment

--- src/common/analysis/faro.js
+++ src/common/analysis/faro.js
@@ -3,20 +3,24 @@ import { TracingInstrumentation } from "@grafana/faro-web-tracing";
 import getSessionId from "@/src/common/utils";
 
 export default function setupFaro() {
-    initializeFaro({
-        url: window.publicEnv ? window.publicEnv.TELEMETRY_URL : "", // required, see below
-        app: {
-            name: "arbeidsplassen", // required
-            version: window.publicEnv ? window.publicEnv.APP_VERSION : "", // optional; useful in Grafana to get diff between versions
-            instrumentations: [
-                ...getWebInstrumentations({
-                    captureConsole: true,
-                }),
-                new TracingInstrumentation(),
-            ],
-        },
-        session: {
-            id: getSessionId(),
-        },
-    });
+    if (process.env.NODE_ENV === "production") {
+        initializeFaro({
+            url: window.publicEnv ? window.publicEnv.TELEMETRY_URL : "", // required, see below
+            app: {
+                name: "arbeidsplassen", // required
+                version: window.publicEnv ? window.publicEnv.APP_VERSION : "", // optional; useful in Grafana to get diff between versions
+                instrumentations: [
+                    ...getWebInstrumentations({
+                        captureConsole: true,
+                    }),
+                    new TracingInstrumentation(),
+                ],
+            },
+            session: {
+                id: getSessionId(),
+            },
+        });
+    } else {
+        console.log("dev environment detected -> disabling faro");
+    }
 }
