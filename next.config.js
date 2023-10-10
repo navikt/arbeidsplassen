dbf4e234ce280a8d4990f7fc05882e4377a099bf
Khiem Diep <khiem.harald.tu.diep@nav.no>
2023-10-10 14:57:22 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -42,27 +42,22 @@ const nextConfig = {
 module.exports = nextConfig;
 
 const { withSentryConfig } = require("@sentry/nextjs");
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-
-module.exports = (phase) => {
-    if (phase === PHASE_DEVELOPMENT_SERVER) {
-        console.log("dev environment detected -> disabling sentry");
-        return nextConfig;
-    } else {
-        return withSentryConfig(
-            module.exports,
-            {
-                silent: true,
-                org: "nav",
-                project: "arbeidsplassen",
-                url: "https://sentry.gc.nav.no/",
-            },
-            {
-                widenClientFileUpload: true,
-                tunnelRoute: "/monitoring",
-                hideSourceMaps: true,
-            },
-        );
-    }
-};
+module.exports =
+    process.env.NODE_ENV === "production"
+        ? withSentryConfig(
+              module.exports,
+              {
+                  silent: true,
+                  org: "nav",
+                  project: "arbeidsplassen",
+                  url: "https://sentry.gc.nav.no/",
+              },
+              {
+                  widenClientFileUpload: true,
+                  tunnelRoute: "/monitoring",
+                  hideSourceMaps: true,
+              },
+          )
+        : nextConfig;
