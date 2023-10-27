f61eac3ae7fd776879a1a76ea894555c8d1b3b5f
otenav <ola.toft-eriksen@nav.no>
2023-10-27 11:37:26 +0200

    Oppdaterer design på siden for å velge rolle

--- src/pages/velg-rolle.jsx
+++ src/pages/velg-rolle.jsx
@@ -1,4 +1,4 @@
-import { Heading, LinkPanel } from "@navikt/ds-react";
+import { Heading, LinkPanel, Stack } from "@navikt/ds-react";
 import Layout from "@/src/common/components/layout/Layout";
 import Head from "next/head";
 
@@ -8,22 +8,24 @@ export default function velgRolle() {
             <Head>
                 <title>Logg inn - arbeidsplassen.no</title>
             </Head>
-            <div className="container-small page-margin-top-and-bottom">
-                <Heading size="xlarge" level="1" spacing>
+            <div className="container-medium page-margin-top-and-bottom">
+                <Heading size="xlarge" level="1" className="mb-8">
                     Er du jobbsøker eller arbeidsgiver?
                 </Heading>
-                <LinkPanel href="../oauth2/login?redirect=/minside" className="arb-link-panel-tertiary mb-4">
-                    <LinkPanel.Title>Jeg er jobbsøker</LinkPanel.Title>
-                    <LinkPanel.Description>Logg inn på min side</LinkPanel.Description>
-                </LinkPanel>
+                <Stack direction={{ xs: "column", md: "row" }} gap="4">
+                    <LinkPanel href="../oauth2/login?redirect=/minside" className="arb-link-panel-secondary flex-1">
+                        <LinkPanel.Title>Jeg er jobbsøker</LinkPanel.Title>
+                        <LinkPanel.Description>Logg inn på min side</LinkPanel.Description>
+                    </LinkPanel>
 
-                <LinkPanel
-                    href="../oauth2/login?redirect=/stillingsregistrering"
-                    className="arb-link-panel-tertiary mb-16"
-                >
-                    <LinkPanel.Title>Jeg er arbeidsgiver</LinkPanel.Title>
-                    <LinkPanel.Description>Logg inn på min bedriftsside</LinkPanel.Description>
-                </LinkPanel>
+                    <LinkPanel
+                        href="../oauth2/login?redirect=/stillingsregistrering"
+                        className="arb-link-panel-secondary flex-1"
+                    >
+                        <LinkPanel.Title>Jeg er arbeidsgiver</LinkPanel.Title>
+                        <LinkPanel.Description>Logg inn på min bedriftsside</LinkPanel.Description>
+                    </LinkPanel>
+                </Stack>
             </div>
         </Layout>
     );
