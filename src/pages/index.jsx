1ed8e2e38337c11334aa4ae787fb7b30d5d2218c
otenav <ola.toft-eriksen@nav.no>
2024-02-12 11:25:47 +0100

    Legger til google-site-verification slik at vi kan bruke Google Search Console

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,7 @@ export default function Page() {
         <Layout>
             <Head>
                 <title>Arbeidsplassen - arbeidsplassen.no</title>
+                <meta name="google-site-verification" content="I1DCqGuh-OEl_WXTcLI7NaNdS4-MjGo9nS_g2OQoajo" />
             </Head>
             <MainPage />
         </Layout>
