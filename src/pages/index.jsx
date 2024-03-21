b72325f97c80010cca9bc607406231cde1979023
otenav <ola.toft-eriksen@nav.no>
2024-03-21 19:40:59 +0100

    Fiks metadata for forsiden

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,8 @@ import Layout from "@/src/common/components/layout/Layout";
 
 export const metadata = {
     title: "Arbeidsplassen.no - alt av arbeid, samlet på én plass",
+    description:
+        "Finn din neste jobb i en av Norges største samlinger av stillinger. Her finner du jobber fra alle bransjer i markedet.",
 };
 
 export default function Page() {
@@ -11,6 +13,7 @@ export default function Page() {
         <Layout>
             <Head>
                 <title>{metadata.title}</title>
+                <meta name="description" content={metadata.description} />
                 <meta name="google-site-verification" content="I1DCqGuh-OEl_WXTcLI7NaNdS4-MjGo9nS_g2OQoajo" />
             </Head>
             <Home />
