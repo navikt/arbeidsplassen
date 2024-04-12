17afdc5dd03f397901468fa4da08a3bb88cb5a24
otenav <ola.toft-eriksen@nav.no>
2024-04-12 10:45:14 +0200

    Fiks menu

--- src/pages/jobbtreff/[[...slug]].jsx
+++ src/pages/jobbtreff/[[...slug]].jsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function Page() {
     return (
-        <Layout variant="company">
+        <Layout>
             <Head>
                 <title>{metadata.title}</title>
                 <meta name="robots" content="noindex,nofollow" />
