990463f8a40e36b97b0a86b0f93f7c1cc21eeb54
Karl J. Overå <k@rl.run>
2024-04-25 15:13:50 +0200

    bytt ut next-auth-wonderwall med oasis
    fixes #109

--- src/pages/api/isAuthenticated.js
+++ src/pages/api/isAuthenticated.js
@@ -1,11 +1,11 @@
-import { validateIdportenToken } from "@navikt/next-auth-wonderwall";
+import { validateIdportenToken } from "@navikt/oasis";
 
 export default async function handler(req, res) {
     const bearerToken = req.headers.authorization;
     if (bearerToken) {
         try {
             const validToken = await validateIdportenToken(bearerToken);
-            if (validToken) {
+            if (validToken.ok) {
                 res.status(200).send("OK");
             } else {
                 res.status(401).send("Unauthorized");
