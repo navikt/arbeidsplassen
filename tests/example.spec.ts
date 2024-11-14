86f5c276db23f774e8ee29a9d0925dc62391bc92
Khiem Diep <khiem.harald.tu.diep@nav.no>
2024-11-14 13:28:56 +0100

    add simple tests run:tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-    await page.goto("https://playwright.dev/");
+    await page.goto("/");
 
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
+    await expect(page).toHaveTitle("Arbeidsplassen.no - alt av arbeid, samlet på én plass");
 });
 
 test("get started link", async ({ page }) => {
-    await page.goto("https://playwright.dev/");
+    await page.goto("/");
 
     // Click the get started link.
-    await page.getByRole("link", { name: "Get started" }).click();
+    await page.getByRole("link", { name: "Søk etter din neste jobb" }).click();
 
     // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
+    await expect(page).toHaveURL(`/stillinger`);
 });
