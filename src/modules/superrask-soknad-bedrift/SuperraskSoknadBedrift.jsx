413759e92316b390cf9ccb63a3edca33fc0af08f
Minh Ha Do <minh.ha.do@nav.no>
2024-12-12 15:35:02 +0100

    Update article text

--- src/modules/superrask-soknad-bedrift/SuperraskSoknadBedrift.jsx
+++ src/modules/superrask-soknad-bedrift/SuperraskSoknadBedrift.jsx
@@ -7,11 +7,12 @@ export default function SuperraskSoknadBedrift() {
         <article>
             <div className="container-small mt-5 mb-12">
                 <Heading size="xlarge" level="1" spacing>
-                    Superrask søknad – en enklere måte å komme i kontakt med relevante jobbsøkere
+                    Nye funksjoner i Superrask søknad – Det er nå enda enklere å følge opp og sortere søknadene du har
+                    mottatt
                 </Heading>
 
                 <BodyLong size="large" spacing>
-                    Superrask søknad er en ny tjeneste på arbeidsplassen.no som vil gjøre rekrutteringen enklere for
+                    Superrask søknad er en tjeneste på arbeidsplassen.no som vil gjøre rekrutteringen enklere for
                     bedrifter og for de som er på jakt etter ny jobb.
                 </BodyLong>
             </div>
@@ -22,15 +23,19 @@ export default function SuperraskSoknadBedrift() {
 
             <div className="container-small mb-16">
                 <BodyLong spacing>
-                    Arbeidplassen.no er en åpen møteplass for alle på arbeidsmarkedet, eid av Nav. Vårt mål er at
-                    bedrifter og jobbsøkere skal finne hverandre, enkelt og effektivt.
-                </BodyLong>
-                <BodyLong className="mb-12">
                     Produktleder for arbeidsplassen.no, Marianne Garmann Ullsand, er veldig fornøyd med at superrask
-                    søknad er på plass. – Ved å velge superrask søknad på arbeidsplassen.no håper vi at bedrifter kan
-                    spare verdifull tid når de skal ansette nye medarbeidere. Det blir også enklere for jobbsøker å vise
-                    at de er interessert i jobben. Vi tror derfor at superrask søknad vil bidra til at bedriftene får
-                    større og raskere respons på sine ledige stillinger, sier hun.
+                    søknad er så godt mottatt. – Vi ser at mange bedrifter og jobbsøkere har tatt i bruk superrask
+                    søknad på arbeidsplassen.no. Bedrifter setter pris på at den er enkel å opprette og at den bidrar
+                    til at de får større og raskere respons på sine ledige stillinger. Jobbsøkerne liker at de enklere
+                    og raskere kan vise at de er interessert i jobben uten CV og skrive lang søknad.
+                </BodyLong>
+                <Heading size="medium" level="2" spacing>
+                    Ny og forbedret versjon
+                </Heading>
+                <BodyLong className="mb-12">
+                    Arbeidsgivere har savnet å kunne sette flere statuser på kandidaten under rekrutteringsprosessen.
+                    Dette, samt flere andre forbedringer og forenklinger, er nå tilgjengelig i den oppdaterte versjonen
+                    av superrask søknad sier hun.
                 </BodyLong>
 
                 <Heading size="large" level="2" spacing>
@@ -43,8 +48,9 @@ export default function SuperraskSoknadBedrift() {
                 <ul>
                     <li>
                         <BodyLong>
-                            Velg superrask søknad når dere registrerer en stillingsannonse. Spesifiser hvilke
-                            kvalifikasjoner dere har behov for, få med må-krav om dere har det.
+                            Velg superrask søknad når dere registrerer eller endrer en stillingsannonse på
+                            arbeidsplassen.no. Spesifiser hvilke kvalifikasjoner dere har behov for, få med må-krav om
+                            dere har det.
                         </BodyLong>
                     </li>
                     <li>
@@ -61,27 +67,30 @@ export default function SuperraskSoknadBedrift() {
                 <ul>
                     <li>
                         <BodyLong>
-                            Dere ser raskt om en søker er aktuell, og velger om dere vil ta kontakt eller ikke. Dere har
-                            ikke inngått noen forpliktelser.
+                            Dere finner søknadene under Stillingsannonser og Vis søknader på den aktuelle stillingen.
+                            Navnene på søkerne vises ikke i utgangspunktet, da det er frivilling å legge inn navn. Dere
+                            kan velge å vise navn.
                         </BodyLong>
                     </li>
                     <li>
                         <BodyLong>
-                            I listen med søknader vises ikke navnene i utgangspunktet, men dere kan velge å se navnet
-                            hvis dere ønsker. Det er frivillig for jobbsøker å legge inn navn.
+                            Dere ser raskt om en søker er aktuell, og velger om dere vil ta kontakt eller ikke.
                         </BodyLong>
                     </li>
                     <li>
                         <BodyLong>
-                            Om dere synes at søkeren er aktuell kan dere velge å få tilgang til kontaktinformasjonen.
+                            Om dere synes at søkeren er aktuell, kan dere velge å få tilgang til kontaktinformasjonen.
                             Jobbsøkeren får e-post om at dere er interessert og sannsynligvis kommer til å ta kontakt.
                         </BodyLong>
                     </li>
                     <li>
+                        <BodyLong> Dere kan endre status på kandidaten underveis i prosessen.</BodyLong>
+                    </li>
+                    <li>
                         <BodyLong spacing>
-                            Dersom dere ikke synes at en jobbsøker passer for stillingen, kan dere enkelt gi beskjed om
-                            dette. Søknaden vil da slettes fra listen og jobbsøkeren får automatisk et vennlig avslag på
-                            sin e-post med arbeidsplassen.no som avsender.
+                            Ønsker dere ikke å gå videre med kandidaten, kan dere enkelt gi beskjed om dette. Søknaden
+                            vil da slettes fra listen og jobbsøkeren får automatisk et vennlig avslag på sin e-post med
+                            arbeidsplassen.no som avsender.
                         </BodyLong>
                     </li>
                 </ul>
@@ -99,13 +108,21 @@ export default function SuperraskSoknadBedrift() {
                     </li>
                 </ul>
 
+                <Heading size="medium" level="3" spacing>
+                    Fikk du ansatt?
+                </Heading>
+                <ul>
+                    <li>
+                        <BodyLong spacing>
+                            Da markerer du stillingen som besatt og annonsen blir avpublisert på arbeidsplassen.no. Vi
+                            sender melding til alle kandidater som ikke har status «ansatt».
+                        </BodyLong>
+                    </li>
+                </ul>
+
                 <BodyLong spacing>
                     Opplysningene som jobbsøkeren har gitt, slettes automatisk 3 måneder etter at fristen i
                     stillingsannonsen har gått ut.
-                </BodyLong>
-                <BodyLong spacing>
-                    Superrask søknad er en ny løsning for bedrifter som registrerer stillinger på arbeidsplassen.no.
-                    Tjenesten er under utvikling og vi vil gjerne ha tilbakemelding på hvordan den fungerer.
                 </BodyLong>
                 <BodyLong className="mb-12">
                     Lykke til med å finne deres neste medarbeider med superrask søknad!
