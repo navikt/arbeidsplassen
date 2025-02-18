b40ced5084895eb1ad2e87538fd1fd4982d659ee
Khiem <112953341+dtkhiem@users.noreply.github.com>
2025-02-18 08:43:41 +0100

    set sessionId duration to max 24 hours deploy:dev (#200)

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,17 +1,27 @@
 import { v4 as uuidv4 } from "uuid";
 
-export const FIELD_SESSION_ID = "sessionId";
+const SESSION_TAG = "session";
+const SESSION_MAX_DURATION_MS = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
 export default function getSessionId() {
-    let sessionId = null;
+    let session = {};
     try {
-        sessionId = sessionStorage.getItem(FIELD_SESSION_ID);
-        if (sessionId === null) {
-            sessionId = uuidv4();
-            sessionStorage.setItem(FIELD_SESSION_ID, sessionId);
+        const sessionString = sessionStorage.getItem(SESSION_TAG);
+        if (sessionString) {
+            session = JSON.parse(sessionString);
         }
+
+        const now = Date.now();
+        if (!session || now - session.timestamp > SESSION_MAX_DURATION_MS) {
+            session = {
+                id: uuidv4(),
+                timestamp: now,
+            };
+            sessionStorage.setItem(SESSION_TAG, JSON.stringify(session));
+        }
+        return session.id;
     } catch (e) {
         // It's possible user has disabled persistent data
-        sessionId = "undefined";
+        return "undefined";
     }
-    return sessionId;
 }
