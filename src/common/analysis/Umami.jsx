faa23c9af31c5958407193314a4de46412a6a175
Audun Skjelvan <audun.skjelvan@nav.no>
2025-02-25 08:52:13 +0100

    add code for production

--- src/common/analysis/Umami.jsx
+++ src/common/analysis/Umami.jsx
@@ -7,12 +7,19 @@ import { CookieBannerUtils } from "@navikt/arbeidsplassen-react";
 
 export default function Umami() {
     const [isDev, setIsDev] = useState(false);
+    const [isProd, setIsProd] = useState(false);
+    const [websiteId, setWebsiteId] = useState("1cc70e4f-bb41-4d28-8115-cbbc32bee4d3");
     const [isAnalyticsEnabled, setIsAnalyticsEnabled] = useState(false);
     const { showCookieBanner } = useContext(CookieBannerContext);
 
     useEffect(() => {
         if (window?.location?.hostname === "arbeidsplassen.intern.dev.nav.no") {
             setIsDev(true);
+        }
+
+        if (process.env.NODE_ENV === "production") {
+            setIsProd(true);
+            setWebsiteId("c2f0a46d-a5b4-4370-8b80-b9b9fcd39f96");
         }
         const consentValues = CookieBannerUtils.getConsentValues();
         setIsAnalyticsEnabled(consentValues.analyticsConsent);
@@ -26,7 +33,7 @@ export default function Umami() {
         }
     }, [showCookieBanner]);
 
-    if (!isDev) {
+    if (!isDev && !isProd) {
         return null;
     }
 
@@ -39,7 +46,7 @@ export default function Umami() {
             defer
             src="https://cdn.nav.no/team-researchops/sporing/sporing.js"
             data-host-url="https://umami.nav.no"
-            data-website-id="1cc70e4f-bb41-4d28-8115-cbbc32bee4d3"
+            data-website-id={websiteId}
             data-domains="arbeidsplassen.intern.dev.nav.no"
         />
     );
