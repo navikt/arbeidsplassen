d7f1ee9184bd913639c3d2cd85a5baef7e06c6d2
otenav <ola.toft-eriksen@nav.no>
2025-03-06 15:54:49 +0100

    Fjerner artikler fra forsiden

--- src/modules/(home)/Home.jsx
+++ src/modules/(home)/Home.jsx
@@ -1,7 +1,6 @@
 import { Bleed, BodyLong, Box, Button, Heading, HStack, Show } from "@navikt/ds-react";
 import { FiguresSideBySide } from "@navikt/arbeidsplassen-react";
 import InformationUkraine from "@/src/modules/(home)/InformationUkraine";
-import ImageLinkPanelSmall from "@/src/common/components/ImageLinkPanelSmall";
 import ImageBannerLinkPanel from "@/src/common/components/ImageBannerLinkPanel";
 
 export default function Home() {
@@ -55,33 +54,6 @@ export default function Home() {
                         <InformationUkraine />
                     </Box>
                 </Bleed>
-            </div>
-
-            <div className="image-link-panel-grid-small mb-12">
-                <ImageLinkPanelSmall
-                    href="/nytt-sokefelt"
-                    image="/images/ansatt.png"
-                    alt="En glad person som står i en butikk."
-                    title="Nytt søkefelt - enklere, raskere og mer fleksibelt!"
-                    description="Nå kan du kombinere fritekst og filtre for å finne akkurat jobben du er ute etter."
-                    color="primary"
-                />
-                <ImageLinkPanelSmall
-                    image="/images/jobbtreff.jpg"
-                    alt="Bilde av person med laptop"
-                    title="Enklere å finne jobber som kan passe"
-                    description="Vi bruker kunstig intelligens til å plassere annonsen i den kategorien som den (mest sannsynlig) hører hjemme i."
-                    href="/nye-filtre"
-                    color="tertiary"
-                />
-                <ImageLinkPanelSmall
-                    href="jobbe-i-utlandet"
-                    image="/images/paris.jpg"
-                    alt="Bilde av Eiffeltårnet"
-                    title="Jobbe i utlandet?"
-                    description="Den Europeiske Jobbmobilitetsportalen (EURES) er et tilbud til deg som ønsker å finne en jobb i EU-/EØS-området og Sveits."
-                    color="secondary"
-                />
             </div>
         </div>
     );
