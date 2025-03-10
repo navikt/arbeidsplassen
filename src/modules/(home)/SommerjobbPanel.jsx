80568b89c2a60aa32dd4f8f3f5bafe46a75f9b88
otenav <ola.toft-eriksen@nav.no>
2025-03-10 10:50:40 +0100

    images

--- src/modules/(home)/SommerjobbPanel.jsx
+++ src/modules/(home)/SommerjobbPanel.jsx
@@ -1,23 +1,22 @@
 import { BodyLong, Box, Heading, HGrid } from "@navikt/ds-react";
 import { ArrowRightIcon } from "@navikt/aksel-icons";
 import Link from "next/link";
+import Image from "next/image";
 
 function SommerjobbPanel() {
     const randomImg = [
-        "/images/a-street-in-a-town.png",
-        "/images/ice-cream-shop.png",
-        "/images/kayakers-on-water.png",
-        "/images/lofoten-photo.png",
-        "/images/seagull-on-ledge.png",
+        "/images/a-street-in-town.jpg",
+        "/images/ice-cream-shop.jpg",
+        "/images/kayakers-on-water.jpg",
+        "/images/lofoten-photo.jpg",
+        "/images/seagull-on-ledge.jpg",
     ][Math.floor(Math.random() * 4)];
 
     return (
         <Link href="/sommerjobb" className="box-link">
             <Box background="surface-alt-1" padding={{ xs: "6", lg: "12" }} borderRadius="medium">
                 <HGrid gap={{ xs: "6", lg: "12" }} columns={{ xs: "1", md: "auto 1fr" }} align="center">
-                    <div>
-                        <img className="box-link-image" src={randomImg} height="316" width="504" alt="Sommeridyll" />
-                    </div>
+                    <Image className="box-link-image" src={randomImg} height="316" width="504" alt="Sommeridyll" />
                     <div>
                         <Heading spacing level="2" size="large">
                             Sommerjobben 2025
