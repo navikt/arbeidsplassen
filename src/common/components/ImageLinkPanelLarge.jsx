e2318ff8f4ac0e2a10e9808f2e08da3c4bb5f06e
otenav <ola.toft-eriksen@nav.no>
2025-04-01 18:37:58 +0200

    Remove args spread

--- src/common/components/ImageLinkPanelLarge.jsx
+++ src/common/components/ImageLinkPanelLarge.jsx
@@ -2,13 +2,13 @@ import { LinkPanel } from "@navikt/ds-react";
 import PropTypes from "prop-types";
 import NextLink from "next/link";
 
-function ImageLinkPanelLarge({ href, image, alt, title, description, color = "primary-solid", ...args }) {
+function ImageLinkPanelLarge({ href, image, alt, title, description, color = "primary-solid" }) {
     return (
         <div className="image-link-panel-large">
             <div className="image-link-panel-img-large">
                 <img src={image} alt={alt} />
             </div>
-            <LinkPanel as={NextLink} className={`arb-link-panel-${color} image-link-panel-link`} href={href} {...args}>
+            <LinkPanel as={NextLink} className={`arb-link-panel-${color} image-link-panel-link`} href={href}>
                 <LinkPanel.Title className="navds-link-panel__title navds-heading--small image-link-panel-content">
                     {title}
                 </LinkPanel.Title>
--- src/common/components/ImageLinkPanelMedium.jsx
+++ src/common/components/ImageLinkPanelMedium.jsx
@@ -2,13 +2,13 @@ import { LinkPanel } from "@navikt/ds-react";
 import PropTypes from "prop-types";
 import NextLink from "next/link";
 
-function ImageLinkPanelMedium({ href, image, alt, title, description, color = "primary-solid", ...args }) {
+function ImageLinkPanelMedium({ href, image, alt, title, description, color = "primary-solid" }) {
     return (
         <div className="image-link-panel-medium">
             <div className="image-link-panel-img-medium">
                 <img src={image} alt={alt} />
             </div>
-            <LinkPanel as={NextLink} className={`arb-link-panel-${color} image-link-panel-link`} href={href} {...args}>
+            <LinkPanel as={NextLink} className={`arb-link-panel-${color} image-link-panel-link`} href={href}>
                 <LinkPanel.Title className="navds-link-panel__title navds-heading--small image-link-panel-content">
                     {title}
                 </LinkPanel.Title>
